336b1bb7d88c28a6dc528dd9363175eca77a08a0
silvaphb <jeferson.aps170@gmail.com>
2026-07-14 08:52:46 -0300

    final changes

---
+++ README.md
@@ -0,0 +1,16 @@
+# Bibliotech
+Foco em trazer eficiência e facilidade nas bibliotecas de escolas. Tornando mais pratico a gerencia de livros para sites e gestão de biblioteca.
+
+# Conexões
+**Get:**
+- `/api/livros`: Retorna a lista de livros em formato JSON.
+-  `/api/livro/{id}`: Retorna determinado livro associado ao ID passado no link. Em caso de inexistência, retorna uma mensagem do erro.
+
+**Post:**
+- `/api/livro`: Necessário passar o Schema (Json/Dict) associado a Model Book. Caso já exista um livro com o mesmo título será retornado o valor False como erro.
+
+**Delete:**
+- `/api/remover_livro`: Necessário passar o ID referente ao livro. Caso não encontre nenhum será retornado False como erro.
+
+**Patch:**
+- `/api/atualizar_livro`: Necessário passar o ID e o Schema (BookIn) com os dados que deseja atualizar. Caso não encontre nenhum livro será retornado o valor False como erro.
--- livraria/api.py
+++ livraria/api.py
@@ -11,43 +11,43 @@ api = NinjaAPI()
 @api.get('/livros', response={200: List[BookOut]})
 def view_books(request):
     books = list(Book.objects.all().values())
-    return books
+    return 200, books
 
-@api.post('/livro', response={201: BookOut})
+@api.post('/livro', response={201: BookOut, 404: dict})
 def new_book(request, book: BookIn):
     try:
         if Book.objects.filter(title=book.title).exists():
-            return {'Error': 'Livro já existente!'}
+            return 404, {'Error': 'Livro já existente!'}
         new_book = Book(title=book.title, author=book.author, description=book.description, publication=book.publication, category=book.category, stock=book.stock)
         new_book.save()
-        return BookOut.from_domain(new_book)
+        return 201, BookOut.from_domain(new_book)
     except Exception as error:
-        return {'Error': error}
+        return 404, {'Error': error}
 
-@api.post('/remover_livro', response={200: bool})
+@api.delete('/remover_livro', response={200: dict, 404: dict})
 def delete_book(request, id: int):
     try:
         if not Book.objects.filter(id=id).exists():
-            return False
+            return 404, {'Error': 'Não existe este livro'}
         
         book = Book.objects.get(id=id)
         book.delete()
-        return True
+        return 200, {'Success': True}
     except Exception as error:
-        return False
+        return 404, {'Error': error}
 
-@api.patch('/atualizar_livro', response={200: BookOut})
+@api.patch('/atualizar_livro', response={200: BookOut, 404: dict})
 def update_book(request, id: int, book: BookUpdate):
     try:
-        if not Book.objects.filter(id=book.id).exists():
-            return False
+        if not Book.objects.filter(id=id).exists():
+            return 404, {'Error': 'Não existe este livro'}
         
         updated_book = Book.objects.get(id=id)
         for attr, value in book.dict().items():
-            setattr(update_book, attr, value)
+            setattr(updated_book, attr, value)
 
         updated_book.save()
         return BookOut.from_domain(updated_book)
     
     except Exception as error:
-        return False
+        return 404, {'Error': error}
