fef14c995d0ad31c97ede1e7042ff838b4f47e56
silvaphb <jeferson.aps170@gmail.com>
2026-07-16 08:15:30 -0300

    Reorganização de estrutura

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'bibliotech.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'bibliotech.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
